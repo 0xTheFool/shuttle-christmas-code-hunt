c0bf67c3e477884850e8fb09f513cadab5262a83
Paras <paras.b1ank@gmail.com>
2023-12-12 20:05:30 +0530

    Added Day 12

---
+++ src/day12.rs
@@ -0,0 +1,96 @@
+use crate::util::MyError;
+use axum::extract::{Path, State};
+use axum::{debug_handler, Json};
+use chrono::prelude::*;
+use chrono::{DateTime, Utc};
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::time::SystemTime;
+use ulid::Ulid;
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub packets: Arc<Mutex<HashMap<String, SystemTime>>>,
+}
+
+#[debug_handler]
+pub async fn save_string(Path(string): Path<String>, State(state): State<AppState>) {
+    let time = SystemTime::now();
+    let mut packets = state.packets.lock().expect("Mutex was poisoned");
+
+    packets.insert(string, time);
+}
+
+#[debug_handler]
+pub async fn load_string(
+    Path(string): Path<String>,
+    State(state): State<AppState>,
+) -> Result<String, MyError> {
+    let time = SystemTime::now();
+    let packets = state.packets.lock().expect("Mutex was poisoned");
+    if let Some(last_accessed) = packets.get(&string) {
+        let duration = time.duration_since(*last_accessed).unwrap();
+
+        let result = format!("{}", duration.as_secs());
+        Ok(result)
+    } else {
+        Err(MyError::CustomError(
+            "Given String was not saved".to_string(),
+        ))
+    }
+}
+
+#[debug_handler]
+pub async fn convert_ulids_to_uuids(Json(ulids): Json<Vec<String>>) -> Json<Vec<String>> {
+    let uuids = ulids
+        .iter()
+        .rev()
+        .map(|x| {
+            let ulid = Ulid::from_string(x).unwrap();
+
+            Uuid::from_bytes(ulid.to_bytes()).to_string()
+        })
+        .collect::<Vec<String>>();
+
+    Json(uuids)
+}
+
+#[debug_handler]
+pub async fn get_ulids(
+    Path(given_week_day): Path<u32>,
+    Json(ulids_array): Json<Vec<String>>,
+) -> Json<Value> {
+    let mut christmas_eve = 0;
+    let mut weekday = 0;
+    let mut in_the_future = 0;
+    let mut lsb_is_1 = 0;
+
+    ulids_array
+        .iter()
+        .map(|x| Ulid::from_string(x).unwrap())
+        .for_each(|x| {
+            let date: DateTime<Utc> = x.datetime().into();
+
+            if date.day() == 24 && date.month() == 12 {
+                christmas_eve += 1;
+            }
+            if date.weekday().number_from_monday() == given_week_day + 1 {
+                weekday += 1;
+            }
+            if date > Utc::now() {
+                in_the_future += 1;
+            }
+            if x.0 & 1u128 == 1 {
+                lsb_is_1 += 1;
+            }
+        });
+
+    Json(json!({
+        "christmas eve": christmas_eve,
+        "weekday": weekday,
+        "in the future": in_the_future,
+        "LSB is 1": lsb_is_1
+    }))
+}
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@
  * */
 
 mod day1;
-mod day11;
 mod day4;
 mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day11;
+mod day12;
 mod util;
 
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 use axum::{
     routing::{get, post},
     Router,
@@ -23,6 +25,7 @@ use day4::{calculate_strength, compare_reindeer};
 use day6::count_elfs;
 use day7::{bake_any, cookie_recipe};
 use day8::{pokemon_momentum, pokemon_weight};
+use day12::{load_string,save_string, AppState, convert_ulids_to_uuids,get_ulids};
 use tower_http::services::ServeDir;
 use util::MyError;
 
@@ -36,6 +39,10 @@ async fn get_error() -> MyError {
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
+    let state = AppState {
+        packets: Arc::new(Mutex::new(HashMap::new())),
+    };
+
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(get_error))
@@ -48,7 +55,12 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/8/weight/:pokemon_number", get(pokemon_weight))
         .route("/8/drop/:pokemon_number", get(pokemon_momentum))
         .route("/11/red_pixels", post(get_no_of_red_pixels))
-        .nest_service("/11/assets", ServeDir::new("assets"));
+        .route("/12/save/:string", post(save_string))
+        .route("/12/load/:string", get(load_string))
+        .route("/12/ulids", post(convert_ulids_to_uuids))
+        .route("/12/ulids/:weekday", post(get_ulids))
+        .nest_service("/11/assets", ServeDir::new("assets"))
+        .with_state(state);
 
     Ok(router.into())
 }
